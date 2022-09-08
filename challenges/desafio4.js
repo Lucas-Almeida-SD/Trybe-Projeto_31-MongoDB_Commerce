7d1fe7dadbb2d76a03016e778c44c1bd8f6c6df7
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-08 15:29:18 -0300

    feat: Deasafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,14 @@
+db.produtos
+  .find({
+    vendidos: {
+      $gt: 50,
+      $lt: 100,
+    },
+  }, {
+    _id: false,
+    nome: true,
+    vendidos: true,
+  })
+  .sort({
+    vendidos: 1,
+  });
