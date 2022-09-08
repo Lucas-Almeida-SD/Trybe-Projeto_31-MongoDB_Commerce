d80a9caf5fcf749e840dcbcee3ec6fa37f7bdfba
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-08 15:56:56 -0300

    feat: Deasafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,15 @@
+db.produtos
+.find({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: "calorias",
+      quantidade: {
+        $lt: 500,
+      },
+    },
+  },
+},
+{
+  _id: false,
+  nome: true,
+});
